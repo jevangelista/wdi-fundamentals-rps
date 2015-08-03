fc01d10ff677ac7612d52e126a6426b13ed8d171
Jamie Eula Evangelista <jevangelista@unknown28cfdae4a6fc.att.net>
2015-08-03 14:26:57 -0700

    Removed quotes from functions and removed the definition part

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,16 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return var move = /* Your Value */;
-    move || 'getInput()';
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return var move = /* Your Value */;
-    move ||'randomPlay()';
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
